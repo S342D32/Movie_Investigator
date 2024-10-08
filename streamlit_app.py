df52c49ae5962bca7ae9fb1b910d31eaa3493a31
Sourajit Nayakl <sourajitnayak3471@gmail.com>
2024-10-08 10:25:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ st.title('🎬🎬Movie Investigator')
 
 
 with st.expander('About us'):
-st.markdown('What thsi app for?')
-st.info('This is a movie investigator app in which you should find which type of movies are good as per rating and user vews.')
-st.warning('Select the genere for the films and start the in vestigation')
+st.markdown('**What this app for?**')
+st.info('This is a movie investigator app in which you should find which type of movies are good as per rating and user views.')
+st.warning('Select the genere for the films and start the investigation')
 
 st.subheader('Which movie genre perform ($)best the box office?')
 
