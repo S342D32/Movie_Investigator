ab3a91d9a32b534b86e6a1aa239ff22b5cc05127
Sourajit Nayakl <sourajitnayak3471@gmail.com>
2024-10-08 10:20:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import altier as alt
 
-st.title('Species name checker')
+st.title('🎬🎬Movie Investigator')
 
-st.write('WellCome to Species name checker')
+st.header('Let's Start the investigation')
+
+
+with st.expander('About us')
+st.markdown('What thsi app for?')
+st.info('This is a movie investigator app in which you should find which type of movies are good as per rating and user vews.')
+
