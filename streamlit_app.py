17f0015184318ef5a4baa5ac3fa2b5bdc80fbee1
Sourajit Nayakl <sourajitnayak3471@gmail.com>
2024-10-08 10:35:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,18 @@ st.title('🎬🎬Movie Investigator')
 
 
 with st.expander('About us'):
-st.markdown('**What this app for?**')
-st.info('This is a movie investigator app in which you should find which type of movies are good as per rating and user views.')
-st.warning('Select the genere for the films and start the investigation')
+  st.markdown('**What this app for?**')
+  st.info('This is a movie investigator app in which you should find which type of movies are good as per rating and user views.')
+  st.warning('Select the genere for the films and start the investigation')
 
-st.subheader('Which movie genre perform ($)best the box office?')
+ st.subheader('Which movie genre perform ($)best the box office?')
+
+df = pd.read_csv('movies_genres_summary.csv')
+df.year = df.year.as_type('int')
+
+genre_list =df.genre.unique()
+genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+
+
+
 
