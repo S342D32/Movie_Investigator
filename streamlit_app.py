c7efb2af5aac669779eb0ca730b8ed400ba24e2c
Sourajit Nayakl <sourajitnayak3471@gmail.com>
2024-10-23 08:03:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,53 @@
+# Import libraries
 import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
 
-st.title('🎬🎬Movie Investigator')
+# Page title
+st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
+st.title('📊 Interactive Data Explorer')
 
+# App description - Explain functionalities in an expander box
+with st.expander('About this app'):
+  st.markdown('**What can this app do?**')
+  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
+  st.markdown('**How to use the app?**')
+  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
 
+# Question header
+st.subheader('Which Movie Genre performs ($) best at the box office?')
 
-with st.expander('About us'):
-  st.markdown('**What this app for?**')
-  st.info('This is a movie investigator app in which you should find which type of movies are good as per rating and user views.')
-  st.warning('Select the genere for the films and start the investigation')
-
-st.subheader('Which movie genre perform ($)best the box office?')
-
-df = pd.read_csv('movies_genres_summary.csv')
+# Load data - Read CSV into a Pandas DataFrame
+df = pd.read_csv('data/movies_genres_summary.csv')
 df.year = df.year.astype('int')
 
-genre_list =df.genre.unique()
+# Genres selection - Create dropdown menu for genre selection
+genres_list = df.genre.unique()
 genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
 
+# Year selection - Create slider for year range selection
+year_list = df.year.unique()
+year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
+year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
 
+# Subset data - Filter DataFrame based on selections
+df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
+reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
+reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
 
+# Editable DataFrame - Allow users to made live edits to the DataFrame
+df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
+                            column_config={"year": st.column_config.TextColumn("Year")},
+                            num_rows="dynamic")
 
+# Data preparation - Prepare data for charting
+df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
+
+# Display line chart
+chart = alt.Chart(df_chart).mark_line().encode(
+            x=alt.X('year:N', title='Year'),
+            y=alt.Y('gross:Q', title='Gross earnings ($)'),
+            color='genre:N'
+            ).properties(height=320)
+st.altair_chart(chart, use_container_width=True)
